a73ae0c204137ca93d6de8eb18bfe2489f2ad0ed
sbrink00 <sbrink00@stuy.edu>
2019-01-03 22:45:40 -0500

    various test commands used while writing

--- driver.java
+++ driver.java
@@ -3,7 +3,11 @@
 //used in the final version.
 public class driver{
   public static void main(String[]args){
-    Card a = new Card(7, 'C');
-    System.out.println(a.getValue());
+    //Card a = new Card(7, 'C');
+    //System.out.println(a.getValue());
+    Deck d1 = new Deck();
+    //System.out.println(d1);
+    Shoe s1 = new Shoe(1);
+    System.out.println(s1);
   }
 }
