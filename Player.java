e97c291250ce8d36243a08729bf8c70bfc78a715
Elias Ferguson <eferguson00@stuy.edu>
2019-01-04 11:28:46 -0500

    added fields and movement method headers

--- Player.java
+++ Player.java
@@ -1,6 +1,14 @@
 public class Player {
+  private int balance;
+  //private int xpos;
+  //private int ypos;
 
-
-
+  //public void moveUp() {}
+  //public void moveDown() {}
+  //public void moveLeft() {}
+  //public void moveRight() {}
+  public void increaseBal(int winnings) {
+    balance += winnings;
+  }
   
 }
