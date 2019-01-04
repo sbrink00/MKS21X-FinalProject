92b85cc99f851db582a1c2f13a5b2df29d1c9354
sbrink00 <sbrink00@stuy.edu>
2019-01-03 21:55:06 -0500

    only has two lines in the main but I used it to test the card class.

---
+++ driver.java
@@ -0,0 +1,9 @@
+//This is a driver that we will use to test individual
+//parts of our project. It won't contain any actual code
+//used in the final version.
+public class driver{
+  public static void main(String[]args){
+    Card a = new Card(7, 'C');
+    System.out.println(a.getValue());
+  }
+}
