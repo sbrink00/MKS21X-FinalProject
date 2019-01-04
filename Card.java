5ec8aa5a651eef179b8f12f29dbe42fea35a87da
sbrink00 <sbrink00@stuy.edu>
2019-01-03 21:47:03 -0500

    added value field so the value of an ace can be changed during blackjack. Added setVal method so this can be accomplished. Added getValue field to access the val

--- Card.java
+++ Card.java
@@ -1,14 +1,26 @@
 //This object represents an individual playing card used in a casino.
 public class Card{
-  private int val;
+  //the number field represents what card it is(4, 5, ace, king, etc...).
+  //the val suit represents what the card's value is.
+  //this field exists because all face cards are worth the same in
+  //blackjack but they might not necessarily be the same card.
+  //it also exists so that the value of aces can be altered without
+  //changing its identity from an ace.
+  private int num, val;
   private char suit;
 
-  public Card(int value, char startSuit){
-    val = value;
+  public Card(int number, char startSuit){
+    num = number;
     suit = startSuit;
+    if (num == 1) val = 11;
+    else if (num <= 10) val = num;
+    else val = 10;
+
   }
 
+  public int getNum() {return num;}
   public int getValue() {return val;}
   public char suit() {return suit;}
+  public void setVal(int newVal) {val = newVal;}
 
 }
