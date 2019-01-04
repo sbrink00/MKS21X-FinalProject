070693c1a036cea20f1c5aebbc4aebe6e00ec225
sbrink00 <sbrink00@stuy.edu>
2019-01-04 11:50:45 -0500

    tried to get special characters but failed

--- driver.java
+++ driver.java
@@ -2,12 +2,16 @@
 //parts of our project. It won't contain any actual code
 //used in the final version.
 public class driver{
-  public static void main(String[]args){
+  /*public static void main(String[]args){
     //Card a = new Card(7, 'C');
     //System.out.println(a.getValue());
     Deck d1 = new Deck();
     //System.out.println(d1);
-    Shoe s1 = new Shoe(1);
-    System.out.println(s1);
-  }
+    Shoe s1 = new Shoe(3);
+    //System.out.println(s1);
+
+  }*/
+  public static void main(String[] args) {
+    System.out.println("\u1F0A1");
+}
 }
