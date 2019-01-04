2e10ff7c1d4e05a012001f040fdc38f5f4aa4d9a
sbrink00 <sbrink00@stuy.edu>
2019-01-03 21:35:21 -0500

    started card class and made constructor that creates the cards value and suit

---
+++ Card.java
@@ -0,0 +1,12 @@
+//This object represents an individual playing card used in a casino.
+public class Card{
+  private int val;
+  private char suit;
+
+  public Card(int value, char startSuit){
+    val = value;
+    suit = startSuit;
+  }
+
+
+}
