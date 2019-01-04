7c3a8cd251f921a80a55612417a58c15a5b76fdc
Elias Ferguson <eferguson00@stuy.edu>
2019-01-04 11:32:11 -0500

    working on constructor

--- Player.java
+++ Player.java
@@ -2,6 +2,14 @@ public class Player {
   private int balance;
   //private int xpos;
   //private int ypos;
+  public Player(int startingBal) {
+    if (startingBal < 1000) {
+      balance = 1000;
+    }
+    else {
+      balance = startingBal;
+    }
+  }
 
   //public void moveUp() {}
   //public void moveDown() {}
