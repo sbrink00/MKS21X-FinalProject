3bcd3370cab32cd724278c19961550867ef67b39
sbrink00 <sbrink00@stuy.edu>
2019-01-05 17:15:12 -0500

    wrote hand class as both the dealer and the player need a hand

---
+++ Hand.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Hand extends CardList.java{
+  private ArrayList<Card> cards;
+  private boolean blackjack;
+  public Hand(){
+    cards = new ArrayList<Card>();
+  }
+}
