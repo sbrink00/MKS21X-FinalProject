e39216af4fc7910857cd2ea39b40fcddf3d988ef
Elias Ferguson <eferguson00@stuy.edu>
2019-01-06 17:39:43 -0500

    completed reel creation in constructor, testing

--- Slots.java
+++ Slots.java
@@ -10,7 +10,12 @@ public class Slots {
     r = new Random();
     reel = new char[45];
     for (int i = 0; i < 45; i++) {
-      
+      if (i == 0) reel[i] = 'J';
+      else if (i > 0 && i < 3) reel[i] = 'A';
+      else if (i > 2 && i < 6) reel[i] = 'B';
+      else if (i > 5 && i < 10) reel[i] = 'C';
+      else if (i > 9 && i < 20) reel[i] = 'D';
+      else reel[i] = 'T'; 
     }
   }
   public void spin() {
