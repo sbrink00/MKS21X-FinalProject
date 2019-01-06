3f81eff9a6b952c8c6e14b799c3eff2df42c3710
Elias Ferguson <eferguson00@stuy.edu>
2019-01-06 17:34:36 -0500

    making progress on constructor

--- Slots.java
+++ Slots.java
@@ -1,5 +1,19 @@
 public class Slots {
-  private Random r1;
-  private Random r2;
-  private Random r3;
+  private Random r;
+  private double prob;
+  private double returnPerc;
+  private double cost;
+  private double jackpot;
+  private double payout;
+  private char[] reel;
+  public Slots(double bet) {
+    r = new Random();
+    reel = new char[45];
+    for (int i = 0; i < 45; i++) {
+      
+    }
+  }
+  public void spin() {
+
+  }
 }
