04be774a8b4686354eb014c80457bb82aabeb018
sbrink00 <sbrink00@stuy.edu>
2019-01-06 15:44:24 -0500

    edited driver to test stuff

--- driver.java
+++ driver.java
@@ -2,16 +2,11 @@
 //parts of our project. It won't contain any actual code
 //used in the final version.
 public class driver{
-  /*public static void main(String[]args){
-    //Card a = new Card(7, 'C');
-    //System.out.println(a.getValue());
-    Deck d1 = new Deck();
-    //System.out.println(d1);
-    Shoe s1 = new Shoe(3);
+  public static void main(String[]args){
+    Shoe s1 = new Shoe(6);
     //System.out.println(s1);
-
-  }*/
-  public static void main(String[] args) {
-    System.out.println("\u1F0A1");
-}
+    Hand h1 = new Hand();
+    h1.add(new Card(3, 'C'));
+    System.out.println(h1);
+  }
 }
