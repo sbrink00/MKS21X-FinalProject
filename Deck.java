7b400be2db9dc8f49fd8dfee5dca500d5d205845
sbrink00 <sbrink00@stuy.edu>
2019-01-07 11:48:28 -0500

    synced up Deck with superclass CardList

--- Deck.java
+++ Deck.java
@@ -1,43 +1,41 @@
 import java.util.Random;
 import java.util.ArrayList;
-public class Deck{
-  private ArrayList<Card> data;
-  Random r;
+public class Deck extends CardList{
 
   public Deck(){
-    data = new ArrayList<Card>();
-    r = new Random();
+    initializeCards();
+    initializeRandom();
     String suits = "CDHS";
     for (int idx = 0; idx < 4; idx ++){
       char current = suits.charAt(idx);
       for (int idx2 = 1; idx2 < 14; idx2 ++){
-        data.add(new Card(idx2, current));
+        add(new Card(idx2, current));
       }
     }
     shuffle();
   }
 
-  public void shuffle(){
-    for (int idx = 0; idx < data.size(); idx ++){
-      int idxToSwapWith = r.nextInt(data.size());
+  /*public void shuffle(){
+    for (int idx = 0; idx < size(); idx ++){
+      int idxToSwapWith = r.nextInt(size());
       swap(idx, idxToSwapWith);
     }
-  }
+  }*/
 
-  public void swap(int idx1, int idx2){
-    Card temp = data.get(idx1);
-    data.set(idx1, data.get(idx2));
-    data.set(idx2, temp);
-  }
+  /*public void swap(int idx1, int idx2){
+    Card temp = get(idx1);
+    set(idx1, get(idx2));
+    set(idx2, temp);
+  }*/
 
-  public ArrayList<Card> data() {return data;}
+  //public ArrayList<Card> data() {return data;}
 
 
-  public String toString(){
+  /*public String toString(){
     String output = "[";
     for (int idx = 0; idx < data.size(); idx ++){
       output += data.get(idx) + ", ";
     }
     return output.substring(0, output.length() - 2) + "]";
-  }
+  }*/
 }
