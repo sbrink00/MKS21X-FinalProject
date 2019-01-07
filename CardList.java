27cbbf48228235546f7974d4f8565bf187229860
sbrink00 <sbrink00@stuy.edu>
2019-01-07 11:48:00 -0500

    added most methods from deck class to CardList so that other card list classes can access them

--- CardList.java
+++ CardList.java
@@ -1,14 +1,43 @@
+import java.util.Random;
 import java.util.ArrayList;
 public abstract class CardList{
 
   private ArrayList<Card> cards;
+  private Random r;
 
+  public void initializeCards() {cards = new ArrayList<Card>();}
+  public void initializeRandom() {r = new Random();}
 
-  public ArrayList<Card> get(){return cards;}
+  public ArrayList<Card> getCards(){return cards;}
+
+  public Card get(int index) {return cards.get(index);}
+
   public void add(Card c) {cards.add(c);}
+
+  public int size() {return cards.size();}
+
+  public Card set(int index, Card c){
+    Card output = cards.get(index);
+    cards.set(index, c);
+    return output;
+  }
+
   public Card remove(Card c){
     cards.remove(c);
     return c;
+  }
+
+  public void swap(int idx1, int idx2){
+    Card temp = get(idx1);
+    set(idx1, get(idx2));
+    set(idx2, temp);
+  }
+
+  public void shuffle(){
+    for (int idx = 0; idx < size(); idx ++){
+      int idxToSwapWith = r.nextInt(size());
+      swap(idx, idxToSwapWith);
+    }
   }
 
   public String toString(){
