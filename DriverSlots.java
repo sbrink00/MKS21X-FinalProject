4643c4096446cf5f1b03e04eb948fb0f1b1b65cb
Elias Ferguson <eferguson00@stuy.edu>
2019-01-07 11:28:23 -0500

    the 95%-99% ratio works

--- DriverSlots.java
+++ DriverSlots.java
@@ -1,12 +1,16 @@
 public class DriverSlots {
   public static void main(String[] args) {
+    double net = 0;
     Player me = new Player(1000000);
     System.out.println(me.getBal());
     SlotsTriple test3 = new SlotsTriple();
-    test3.spin(1000);
-    test3.interpretSpin();
-    me.changeBal(test3.getPayout());
-    System.out.println("Your balance has been changed by: $" + test3.getPayout());
+    for (int i = 0; i < 100; i++) {
+      test3.spin(1000);
+      test3.interpretSpin();
+      net += test3.getPayout();
+      me.changeBal(test3.getPayout());
+    }
+    System.out.println("You balance has been changed by: " + net);
     System.out.println("Your current balance is: $" + me.getBal());
   }
 }
