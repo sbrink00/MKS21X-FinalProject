1fb19a828f7a25df7f301db3b1ef1bf64e351b4e
Elias Ferguson <eferguson00@stuy.edu>
2019-01-07 13:57:00 -0500

    completed for loop in constructor to correspond numbers with isOdd

--- Wheel.java
+++ Wheel.java
@@ -7,6 +7,12 @@ public class Wheel extends Tile {
               13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33,
               1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35,
               3, 26};
+    for (int i = 0; i < 37; i++) {
+      if (values[i] != 0) {
+        if (values[i] % 2 == 0) odds[i] = false;
+        else if (values[i] % 2 == 1) odds[i] = true;
+      }
+    }
 
   }
 }
