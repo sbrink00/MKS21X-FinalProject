86be066d2fbca2874ab81bd6e897f2f3925b33e2
Elias Ferguson <eferguson00@stuy.edu>
2019-01-06 20:40:11 -0500

    fixed typo

--- Player.java
+++ Player.java
@@ -15,8 +15,8 @@ public class Player {
   //public void moveDown() {}
   //public void moveLeft() {}
   //public void moveRight() {}
-  public void increaseBal(int value) {
-    balance += winnings;
+  public void changeBal(int value) {
+    balance += value;
   }
   public int getBal() {
     return balance;
