93e3051ed87bc375e2468c55485415edda7003b2
sbrink00 <sbrink00@stuy.edu>
2019-01-07 18:50:04 -0500

    did more tests as needed

--- driver.java
+++ driver.java
@@ -3,7 +3,7 @@
 //used in the final version.
 public class driver{
   public static void main(String[]args){
-    Deck d1 = new Deck();
-    System.out.println(d1);
+    Shoe s1 = new Shoe(3);
+    System.out.println(s1);
   }
 }
