1f8339771f6eff0302262d9165277b73569d531d
sbrink00 <sbrink00@stuy.edu>
2019-01-07 11:49:53 -0500

    tested out code

--- driver.java
+++ driver.java
@@ -3,10 +3,7 @@
 //used in the final version.
 public class driver{
   public static void main(String[]args){
-    Shoe s1 = new Shoe(6);
-    //System.out.println(s1);
-    Hand h1 = new Hand();
-    h1.add(new Card(3, 'C'));
-    System.out.println(h1);
+    Deck d1 = new Deck();
+    System.out.println(d1);
   }
 }
