308f217fe67e5c2e383f0a1052ee5ee0d3fcf0aa
Elias Ferguson <eferguson00@stuy.edu>
2019-01-07 13:38:14 -0500

    added fields and constructor

---
+++ Tile.java
@@ -0,0 +1,10 @@
+public class Tile {
+  private int val;
+  private char color;
+  private boolean odd;
+  public Tile(int inVal, char inCol, boolean inOdd) {
+    val = inVal;
+    color = inCol;
+    odd = inOdd;
+  }
+}
