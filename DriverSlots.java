6ca24ce3c7eb368d8046dbb8893728c9b39f0f5c
Elias Ferguson <eferguson00@stuy.edu>
2019-01-06 21:17:22 -0500

    driver updated

--- DriverSlots.java
+++ DriverSlots.java
@@ -2,5 +2,11 @@ public class DriverSlots {
   public static void main(String[] args) {
     Player me = new Player(1000000);
     System.out.println(me.getBal());
+    SlotsTriple test3 = new SlotsTriple();
+    test3.spin(1000);
+    test3.interpretSpin();
+    me.changeBal(test3.getPayout());
+    System.out.println("Your balance has been changed by: $" + test3.getPayout());
+    System.out.println("Your current balance is: $" + me.getBal());
   }
 }
