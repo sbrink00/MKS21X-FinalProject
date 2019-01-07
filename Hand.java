9f0f5f6779e16ffd754ea00c5b750a078a1ccb8f
sbrink00 <sbrink00@stuy.edu>
2019-01-06 23:50:55 -0500

    wrote hand class that extends CardList. Have yet to sync up CardList and its sublclasses

--- Hand.java
+++ Hand.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-public class Hand extends CardList.java{
-  private ArrayList<Card> cards;
+public class Hand extends CardList{
+  //private ArrayList<Card> cards;
   private boolean blackjack;
   public Hand(){
     cards = new ArrayList<Card>();
