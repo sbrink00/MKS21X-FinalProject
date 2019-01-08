725ecffea7c1e2ada6cca076844f28cd9d3a5b79
Elias Ferguson <eferguson00@stuy.edu>
2019-01-08 00:06:01 -0500

    added spin() function to return an int between 0 and 36 inclusive

--- Wheel.java
+++ Wheel.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class Wheel {
-  private char[] colors;
-  private int[] values;
-  private boolean[] odds;
+  private ArrayList<Tile> wheel;
+  private Random r;
   public Wheel() {
+    r= new Random();
     values = new int[]{0, 32, 15, 19, 4, 21, 2, 25, 17, 37, 6, 27,
               13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33,
               1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35,
@@ -22,5 +23,8 @@ public class Wheel {
         else colors[i] = 'R';
       }
     }
+  }
+  public int spin() {
+    return (Math.abs(r.nextInt()) % 37);
   }
 }
