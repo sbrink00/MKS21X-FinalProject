29b3ba8764ac5450cc77465b330439c6fda5a397
sbrink00 <sbrink00@stuy.edu>
2019-01-07 19:20:59 -0500

    added instance variables to blackjack class

--- Blackjack.java
+++ Blackjack.java
@@ -6,13 +6,29 @@ public class Blackjack{
       hand = new Hand();
     }
     public Hand get(){return hand;}
-    public
+
   }
 
   private Dealer dealer;
   private Player player;
+  private double bet;
+  //the following strings are going to be used in the run function.
+  //they are acronyms and will be explained with comments after the string
+  private String EB = "Please enter your bet: "; //Enter Bet
+  private String HS = "Do you want to hit or stand?";//Hit or Stand
+  private String CP = "Do you want to continue playing blackjack?";//Continue Playing
+  private String YN = "Enter 'y' for yes and 'n' for no: ";//Yes or No
 
-  public void run(){}
-  
+
+  public Blackjack(){
+    player = new Player();
+    dealer = new Dealer();
+
+  }
+
+  public void run(){
+
+  }
+
 
 }
