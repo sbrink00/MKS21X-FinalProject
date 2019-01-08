723ff79c21e1fbd69c2c1b8687436216d3bd99b8
Elias Ferguson <eferguson00@stuy.edu>
2019-01-08 11:30:56 -0500

    changing whole thing to represent the board instead of the wheel of roulette

--- Wheel.java
+++ Wheel.java
@@ -1,27 +1,17 @@
 import java.util.*;
 public class Wheel {
   private int[] values;
-  private char[] colors;
-  private boolean[] odds;
+  private ArrayList<ArrayList<Tile>> board;
   public Wheel() {
     values = new int[]{0, 32, 15, 19, 4, 21, 2, 25, 17, 37, 6, 27,
               13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33,
               1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35,
               3, 26};
-    for (int i = 0; i < 37; i++) {
-      if (values[i] != 0) {
-        if (values[i] % 2 == 0) odds[i] = false;
-        else if (values[i] % 2 == 1) odds[i] = true;
-      }
-      else if (values[i] == 0) {
-        colors[i] = 'G';
-      }
-      if (i != 0) {
-        if (i % 2 == 0) {
-          colors[i] = 'B';
-        }
-        else colors[i] = 'R';
-      }
+    for (int i = 0; i < 4; i++) {
+      board.add(new ArrayList<Tile>());
     }
+    board.get(0).add(0);
+    int counter = 0;
+    while ()
   }
 }
