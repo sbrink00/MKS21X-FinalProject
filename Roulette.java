0ce7f65f289d1055b08894c9ba8ec0fd8390ed2e
Elias Ferguson <eferguson00@stuy.edu>
2019-01-09 11:19:29 -0500

    wrote spin() command, added a new private field spinBoard that is the same as board but a 1D arrayList

--- Roulette.java
+++ Roulette.java
@@ -2,20 +2,24 @@ import java.util.*;
 public class Roulette {
   private Random r;
   private ArrayList<ArrayList<Tile>> board;
+  private ArrayList<ArrayList<Tile>> spinBoard;
   public Roulette() {
     board = new ArrayList<ArrayList<Tile>>();
     for (int i = 0; i < 4; i++) {
       board.add(new ArrayList<Tile>());
     }
     board.get(0).add(new Tile(0, 'G'));
+    spinBoard.add(new Tile(0, 'G'));
     int counter = 1;
     while (counter < 37) {
       for (int i = 1; i < 4; i++) {
         if (counter % 2 == 0) {
           board.get(i).add(new Tile (counter, 'B'));
+          spinBoard.add(new Tile(couter, 'B'));
         }
         else if (counter % 2 == 1) {
           board.get(i).add(new Tile (counter, 'R'));
+          spinBoard.add(new Tile(counter, 'R'));
         }
         counter++;
       }
@@ -23,7 +27,8 @@ public class Roulette {
     r = new Random();
   }
   public Tile spin() {
-    return (Math.abs(r.nextInt()) % 37);
+    int rando = r.nextInt(37);
+    return spinBoard.get(rando);
   }
   public String printBoard() {
     String output = "";
