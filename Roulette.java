cccf8dfb8b8f798c6cdff8d4286720215320fb0f
Elias Ferguson <eferguson00@stuy.edu>
2019-01-08 20:48:24 -0500

    consolidated RBoard and Roulette classes, will delete RBoard because it is completely irrelevant

--- Roulette.java
+++ Roulette.java
@@ -1,11 +1,28 @@
+import java.util.*;
 public class Roulette {
   private Random r;
-  private Wheel wheel;
+  private ArrayList<ArrayList<Tile>> board;
   public Roulette() {
-    wheel = new Wheel();
+    for (int i = 0; i < 4; i++) {
+      board.add(new ArrayList<Tile>());
+    }
+    board.get(0).add(new Tile(0, 'G'));
+    int counter = 1;
+    while (counter < 37) {
+      for (int i = 1; i < 4; i++) {
+        if (counter % 2 == 0) {
+          board.get(i).add(new Tile (counter, 'B'));
+        }
+        else if (counter % 2 == 1) {
+          board.get(i).add(new Tile (counter, 'R'));
+        }
+        counter++;
+      }
+    }
     r = new Random();
   }
   public int spin() {
     return (Math.abs(r.nextInt()) % 37);
+
   }
 }
