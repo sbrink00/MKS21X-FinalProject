5ba9292adaaed40cfa5540ac0cd367e568cc46bf
Elias Ferguson <eferguson00@stuy.edu>
2019-01-09 11:26:23 -0500

    updated to test spin()

--- RouletteDriver.java
+++ RouletteDriver.java
@@ -2,5 +2,7 @@ public class RouletteDriver {
   public static void main(String[] args) {
     Roulette test = new Roulette();
     System.out.println(test.printBoard());
+    Tile output = test.spin();
+    System.out.println(output.toString());
   }
 }
