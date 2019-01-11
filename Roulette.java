34ed6b0ab96d4b083a9427616f373308dc8b0c7a
Elias Ferguson <eferguson00@stuy.edu>
2019-01-10 21:20:46 -0500

    writing betWinsPossibilites(), which will return an array full of all the winning possible integers according to the bet type

--- Roulette.java
+++ Roulette.java
@@ -1,16 +1,17 @@
 import java.util.*;
 public class Roulette {
   private Random r;
+  private double betVal;
   private ArrayList<ArrayList<Tile>> board;
   private ArrayList<Tile> spinBoard;
-  private String[] betInfo;
+  private ArrayList<String> betInfo;
   private Scanner in;
   public Roulette() {
     in = new Scanner(System.in);
-    betInfo = new String[2];
+    betInfo = new ArrayList<String>();
     int counter = 0;
     while (in.hasNext() && counter < 2) {
-      betInfo[counter] = in.next();
+      betInfo.add(in.next());
     }
     board = new ArrayList<ArrayList<Tile>>();
     spinBoard = new ArrayList<Tile>();
@@ -48,14 +49,17 @@ public class Roulette {
     output += "Square (4 Numbers): Returns 9x your bet. Input: <number1, number2, number3, number4>\n";
     output += "Six Line (6 Numbers): Returns 6x. Input: <number1, number2, number3, number4, number5, number6>\n";
     output += "Colors (18 Numbers): Returns 2x your bet. Input: <color>\n";
-    output += "Dozens (12 Numbers): Returns 3x your bet. DozenIDs are (1-12 is 0, 13-24 is 1, 25-36 is 2). Input: <DOZENdozenID>\n";
+    output += "Dozens (12 Numbers): Returns 3x your bet. DozenIDs are (1-12 is 0, 13-24 is 1, 25-36 is 2). Input: <DOZEN dozenID>\n";
     output += "Highs/Lows (18 Numbers): Returns 2x your bet. Preferences are (high, low). Input: <Preference>\n";
     output += "Odds/Evens (18 Numbers): Returns 2x your bet. Preferences are (odd, even). Input: <Preference>\n";
     output += "Columns (12 Numbers): Returns 3x your bet. columnIDs are (Column starting at 1 is 0, column starting at 2 is 1, Column starting at 3 is 2.) Input: <COLUMNcolumnID>\n";
     System.out.println(output);
   }
-  public String betType() {
-    
+  public int[] betWinsPossibilites() {
+    betVal = parseFloat(betInfo.get(0));
+    int[] output;
+    if (betInfo.get(1).equals("high")) output = new int[]{19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
+    else if (betInfo.get(1).equals("low")) output = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
   }
   public double interpretSpin() {
     double output = 0;
