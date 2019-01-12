4de8efcf3c5b08634c8847fcfd31e25acca90178
Elias Ferguson <eferguson00@stuy.edu>
2019-01-12 13:23:52 -0500

    altered driver

--- DriverSlots.java
+++ DriverSlots.java
@@ -1,16 +1,6 @@
 public class DriverSlots {
   public static void main(String[] args) {
-    double net = 0;
-    Player me = new Player(1000000);
-    System.out.println(me.getBal());
-    SlotsTriple test3 = new SlotsTriple();
-    for (int i = 0; i < 100; i++) {
-      test3.spin(1000);
-      test3.interpretSpin();
-      net += test3.getPayout();
-      me.changeBal(test3.getPayout());
-    }
-    System.out.println("You balance has been changed by: " + net);
-    System.out.println("Your current balance is: $" + me.getBal());
+    SlotsTriple test = new SlotsTriple();
+    test.run();
   }
 }
