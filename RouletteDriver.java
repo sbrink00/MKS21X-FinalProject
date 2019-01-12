2ebc8f3370b918af376de2c6bafb4812c015a065
Elias Ferguson <eferguson00@stuy.edu>
2019-01-12 12:21:20 -0500

    user input is broken will address that

--- RouletteDriver.java
+++ RouletteDriver.java
@@ -2,8 +2,6 @@ public class RouletteDriver {
   public static void main(String[] args) {
     Roulette test = new Roulette();
     System.out.println(test.printBoard());
-    Tile output = test.spin();
-    System.out.println(output.toString());
-    displayOptions();
+    test.run();
   }
 }
