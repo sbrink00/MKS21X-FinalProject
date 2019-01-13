7bbf23470ed9d6bf02afb79a6fe515fc74ad9526
sbrink00 <sbrink00@stuy.edu>
2019-01-12 20:25:55 -0500

    added methods in Hand class to check whether the hand is blackjack and if it's splittable

--- Hand.java
+++ Hand.java
@@ -4,5 +4,17 @@ public class Hand extends CardList{
   private boolean blackjack;
   public Hand(){
     initializeCards();
+    blackjack = false;
   }
+
+  public boolean isBlackjack(){
+    if (size() == 2 && sumValues() == 21) return true;
+    return false;
+  }
+
+  public boolean splittable(){
+    if (size() == 2 && get(0).equalsNumber(get(1))) return true;
+    return false;
+  }
+
 }
