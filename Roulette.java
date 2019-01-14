1fba906c9a4d43c40ada8048aedb6d75bff2b12a
Elias Ferguson <eferguson00@stuy.edu>
2019-01-13 22:49:02 -0500

    incorporated columnBet and dozenBet into the betWinsPossibilites() method

--- Roulette.java
+++ Roulette.java
@@ -69,14 +69,16 @@ public class Roulette {
     else if (betType.equals("odd")) winners = new int[]{1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35};
     else if (betType.equals("even")) winners = new int[]{2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36};
     else if (betType.equals("dozen")) {
-      if (betInfo.get(2).equals("0")) winners = new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
-      else if (betInfo.get(2).equals("1")) winners = new int[]{13,14,15,16,17,18,19,20,21,22,23,24};
-      else if (betInfo.get(2).equals("2")) winners = new int[]{25,26,27,28,29,30,31,32,33,34,35,36};
+      dozenBet();
+      if (dozenID == 0) winners = new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
+      else if (dozenID == 1) winners = new int[]{13,14,15,16,17,18,19,20,21,22,23,24};
+      else if (dozenID == 2) winners = new int[]{25,26,27,28,29,30,31,32,33,34,35,36};
     }
     else if (betType.equals("column")) {
-      if (betInfo.get(2).equals("0")) winners = new int[]{1,4,7,10,13,16,19,22,25,28,31,34};
-      else if (betInfo.get(2).equals("1")) winners = new int[]{2,5,8,11,14,17,20,23,26,29,32,35};
-      else if (betInfo.get(2).equals("2")) winners = new int[]{3,6,9,12,15,18,21,24,27,30,33,36};
+      columnBet();
+      if (columnID == 0) winners = new int[]{1,4,7,10,13,16,19,22,25,28,31,34};
+      else if (columnID == 1) winners = new int[]{2,5,8,11,14,17,20,23,26,29,32,35};
+      else if (columnID == 2) winners = new int[]{3,6,9,12,15,18,21,24,27,30,33,36};
     }
     else {
       int counter = 1;
