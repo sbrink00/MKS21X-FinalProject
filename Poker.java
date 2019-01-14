157c3ca82da2e5e0dc77b7ac0f9227fb406592f5
Elias Ferguson <eferguson00@stuy.edu>
2019-01-14 14:03:59 -0500

    started adding some fields

---
+++ Poker.java
@@ -0,0 +1,10 @@
+public class Poker {
+  private boolean flush;
+  private boolean royals;
+  private boolean straight;
+  private boolean FoC;
+  private boolean ToC;
+  private boolean pairs;
+  private boolean fullhouse;
+
+}
