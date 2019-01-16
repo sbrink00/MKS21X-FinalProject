4110d89c1399e3b93c6d2e3ff70dfaf87291b023
sbrink00 <sbrink00@stuy.edu>
2019-01-15 22:55:16 -0500

    started working on the master casino file. This will be the only file that will be run by the user

--- Casino.java
+++ Casino.java
@@ -1,1 +1,16 @@
-yeet yah
+import java.util.Scanner;
+public class Casino{
+  public static void main(String[]args){
+    System.out.println("WELCOME TO THE JAVA CASINO. HIT ENTER TO ENTER WITH 1000 DOLLARS!!!!");
+    Scanner scan = new Scanner(System.in);
+    Player player = new Player(1000);
+    String enter = scan.nextLine();
+    System.out.println("You have 1000 dollars. When you have 0, you will be kicked out.");
+    String iUnderstand = scan.nextLine();
+    boolean done = false;
+    while (!done){
+      System.out.println("Would you like to play Roulette, Blackjack, Slots, or Video Poker?");
+      String game = scan.nextLine();
+    }
+  }
+}
