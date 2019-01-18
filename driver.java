1dfb7a558dc4e7958a0f3cf1ed73b1534422718d
sbrink00 <sbrink00@stuy.edu>
2019-01-17 19:31:14 -0500

    updated driver to perform more tests

--- driver.java
+++ driver.java
@@ -1,6 +1,12 @@
 public class driver{
   public static void main(String[]args){
-    Poker p1 = new Poker();
-    p1.run();
+    Hand h1 = new Hand();
+    h1.add(new Card(5, 'S'));
+    h1.add(new Card(3, 'S'));
+    h1.add(new Card(2, 'S'));
+    h1.add(new Card(8, 'S'));
+    h1.add(new Card(7, 'S'));
+    h1.sort();
+    System.out.println(h1);
   }
 }
