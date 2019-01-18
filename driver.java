87994ad856178d7fb95749d823cab8d24afee6ad
sbrink00 <sbrink00@stuy.edu>
2019-01-17 19:34:49 -0500

    pair now correctly returns wheter not hand has a pair

--- driver.java
+++ driver.java
@@ -4,9 +4,9 @@ public class driver{
     h1.add(new Card(5, 'S'));
     h1.add(new Card(3, 'S'));
     h1.add(new Card(2, 'S'));
-    h1.add(new Card(8, 'S'));
+    h1.add(new Card(5, 'S'));
     h1.add(new Card(7, 'S'));
     h1.sort();
-    System.out.println(h1);
+    System.out.println(h1.pair());
   }
 }
