69b81d4150329b46adb7ebb5ca780f6de191b5d5
Elias Ferguson <eferguson00@stuy.edu>
2019-01-18 23:08:32 -0500

    merging

--- CasinoWar.java
+++ CasinoWar.java
@@ -1,11 +1,16 @@
 import java.util.*;
 public class CasinoWar {
+<<<<<<< HEAD
   private Hand playerHand;
+=======
+  private ArrayList<Hand> playerHand;
+>>>>>>> 629bcf6267ee900484dbb3ffab5e695489a75460
   private Dealer dealer;
   private Player player;
   private double bet;
   private Shoe shoe;
   private Scanner in;
+<<<<<<< HEAD
   private double payout;
   private class Dealer {
     private Hand dealerHand;
@@ -27,4 +32,12 @@ public class CasinoWar {
   public void interpretDeal() {
     if playerHand.get(0).getValue()
   }
+=======
+  private class Dealer {
+    private Hand hand;
+    public Dealer() {
+      hand = new Hand();
+    }
+  }
+>>>>>>> 629bcf6267ee900484dbb3ffab5e695489a75460
 }
